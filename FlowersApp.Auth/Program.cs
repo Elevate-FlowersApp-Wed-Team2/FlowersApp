29fb64d0e49ed125f751347430af895ecedbcef0
Manar-Nabil <manar.nabil@business-expert.ai>
2026-08-02 20:23:00 +0300

    try

--- FlowersApp.Auth/Program.cs
+++ FlowersApp.Auth/Program.cs
@@ -8,6 +8,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
+
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddHealthChecks()
                .AddCheck("self", () => HealthCheckResult.Healthy());
