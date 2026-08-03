53a191f1cf13318e65b0b1847ca2405160f5a28c
karim salah <karimabdelghany753@gmail.com>
2026-08-04 01:11:57 +0300

    Add Redis integration and shared project for caching
    Integrated Redis caching functionality using `StackExchange.Redis`.
    Added `FlowersApp.Shared` project to encapsulate shared logic,
    including `IRedisCacheService` and its implementation. Updated
    `Program.cs` to load environment variables with `DotNetEnv` and
    register Redis services. Added Redis configuration to
    `appsettings.json`. Updated solution file to include the new
    shared project. Minor updates to `docker-compose.yml` for Redis
    support.

--- FlowersApp.Auth/Program.cs
+++ FlowersApp.Auth/Program.cs
@@ -1,13 +1,16 @@
 
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
-
+using StackExchange.Redis;
+using DotNetEnv;
+using FlowersApp.Shared.Redis;
 namespace FlowersApp.Auth;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        Env.Load();
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddHealthChecks()
                .AddCheck("self", () => HealthCheckResult.Healthy());
@@ -23,6 +26,16 @@ public class Program
         {
             Predicate = check => check.Name == "self"
         });
+
+        //register redis 
+        var redisConnection = builder.Configuration["REDIS_CONNECTION_STRING"]
+                            ?? Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING");
+
+        if (string.IsNullOrEmpty(redisConnection))
+            throw new InvalidOperationException("REDIS_CONNECTION_STRING is not set. Check your .env file.");
+
+        builder.Services.AddRedisCache(redisConnection);
+
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
