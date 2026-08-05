a5f16f3fc2eea06136ebaaa693169e0c312d2d33
Manar-Nabil <manar.nabil@business-expert.ai>
2026-08-05 09:56:06 +0300

    Add driver application feature with MediatR & validation
    Introduce driver application submission using MediatR and FluentValidation, including command, handler, and validator. Add shared command/query interfaces, a generic repository, and standardized request result types. Update dependencies and set EF Core to NoTracking for queries. Remove unused Features folder reference.

---
+++ FlowersApp.Auth/Extensions/ApplicationDependanciesRegister.cs
@@ -0,0 +1,14 @@
+﻿
+namespace FlowersApp.Auth.Extensions;
+
+public static class ApplicationDependanciesRegister
+{
+    public static IServiceCollection AddApplicationDependancies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(ApplicationDependanciesRegister).Assembly)
+        });
+        return services;
+    }
+}
