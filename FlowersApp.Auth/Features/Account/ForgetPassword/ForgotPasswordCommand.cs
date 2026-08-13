1e1e88e7939668eb78b1799562e55ef753cf0de9
karim salah <karimabdelghany753@gmail.com>
2026-08-13 06:58:09 +0300

    feat: add OtpVerificationStatus enum and remove old Account folder

--- FlowersApp.Auth/Features/Account/ForgetPassword/ForgotPasswordCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using FlowersApp.Auth.Shared.Interfaces;
-using MediatR;
-using Superpower.Model;
-
-namespace FlowersApp.Auth.Features.Account.ForgetPassword
-{
-    public record ForgotPasswordCommand(string Email) : ICommand<ForgotPasswordResponse>;
-    public record ForgotPasswordResponse(string Message);
-}
--- FlowersApp.Auth/Features/Account/ResetPassword/ResetPasswordCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using FlowersApp.Auth.Shared.Interfaces;
-
-namespace FlowersApp.Auth.Features.Account.ResetPassword
-{
-    public record ResetPasswordOrchestrator(
-    string Email, string ResetToken, string NewPassword, string ConfirmNewPassword
-    ) : ICommand<ResetPasswordResponse>;
-    public record ResetPasswordResponse(string Message);
-}
