f2a0ca14b3f6376788277d3ce22aaa8b2868c409
Manar-Nabil <manar.nabil@business-expert.ai>
2026-08-17 22:10:41 +0300

    complte intefaces

---
+++ FlowersApp.Catalog/Shared/Interfaces/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using FlowersApp.Catalog.Shared.Response;
+using MediatR;
+
+namespace FlowersApp.Catalog.Shared.Interfaces;
+
+public interface ICommand<TResult>:IRequest<RequestResult<TResult>>
+{}
+
+public interface ICommandHandler<TCommand, TResult> 
+    : IRequestHandler<TCommand, RequestResult<TResult>>
+    where TCommand : ICommand<TResult>
+{
+}
